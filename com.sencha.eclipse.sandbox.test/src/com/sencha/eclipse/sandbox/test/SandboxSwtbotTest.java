41cf4df17a8deabeb8a0fee9a1fa4abff3ee2174
branflake2267@gmail.com <branflake2267@gmail.com>
2015-05-16 08:21:00 -0700

    Readme update and change swtbot test.

--- com.sencha.eclipse.sandbox.test/src/com/sencha/eclipse/sandbox/test/SandboxSwtbotTest.java
+++ com.sencha.eclipse.sandbox.test/src/com/sencha/eclipse/sandbox/test/SandboxSwtbotTest.java
@@ -5,7 +5,6 @@ package com.sencha.eclipse.sandbox.test;
 
 import com.google.gdt.eclipse.swtbot.SwtBotProjectActions;
 import com.google.gdt.eclipse.swtbot.SwtBotTestingUtilities;
-import com.google.gdt.eclipse.swtbot.SwtBotWorkbenchActions;
 
 import junit.framework.TestCase;
 
@@ -30,8 +29,6 @@ public class SandboxSwtbotTest extends TestCase {
   }
 
   public void testSwtBotCreateProject() {
-    SwtBotWorkbenchActions.waitForMainShellProgressBarToFinish(bot);
-
     SwtBotProjectActions.createJavaProject(bot, "SandboxySwty");
   }
 
--- com.sencha.eclipse.sandbox/README.md
+++ com.sencha.eclipse.sandbox/README.md
@@ -4,3 +4,20 @@ Testing Sandbox
 ## Linkes
 
 * [Build Server](http://146.148.64.240:8080/)
+
+## Testing
+
+* run `mvn verify` to test build
+
+## Errors
+
+### No Display 
+
+* This occurs when the build server is running on a different user and doesn't have access.
+* Try running the build server as the same user as started the Display. 
+
+```
+Launching application org.eclipse.ui.ide.workbench...
+gdk_mir_display_open
+Failed to connect to Mir: Failed to connect to server socket: No such file or directory
+```

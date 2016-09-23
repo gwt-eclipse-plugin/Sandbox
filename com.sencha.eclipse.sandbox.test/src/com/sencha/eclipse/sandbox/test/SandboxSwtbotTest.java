c29d1b67956e4076072de9f4cce48cc75c9d080c
branflake2267@gmail.com <branflake2267@gmail.com>
2016-09-22 17:46:39 -0700

    Fixed fake test

--- com.sencha.eclipse.sandbox.test/src/com/sencha/eclipse/sandbox/test/SandboxSwtbotTest.java
+++ com.sencha.eclipse.sandbox.test/src/com/sencha/eclipse/sandbox/test/SandboxSwtbotTest.java
@@ -7,7 +7,10 @@ import com.sencha.eclipse.sandbox.swtbot.SwtBotTestingUtilities;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.swt.finder.SWTBot;
 
 /**
  * https://wiki.eclipse.org/SWTBot/UsersGuide
@@ -28,16 +31,28 @@ public class SandboxSwtbotTest extends TestCase {
   }
 
   public void testRunSomethingInUi() {
-    // do something in ui
-    SwtBotTestingUtilities.performAndWaitForWindowChange(bot, new Runnable() {
-      @Override
-      public void run() {
-        bot.perspectiveById("org.eclipse.jdt.ui.JavaPerspective").activate();
-      }
-    });
+    waitForIdle(bot);
+
+    //closeWelcome(bot);
 
     // if I got this far, then the linux headless x frame buffer is working
     assertTrue(true);
+  }
+
+  public static void closeWelcome(SWTWorkbenchBot bot) {
+    SWTBotView activeView = bot.activeView();
+    if (activeView != null && activeView.getTitle().equals("Welcome")) {
+      activeView.close();
+    }
+  }
+
+  /**
+   * Wait until all background tasks are complete.
+   */
+  public static void waitForIdle(SWTBot bot) {
+    while (!Job.getJobManager().isIdle()) {
+      bot.sleep(1000);
+    }
   }
 
 }
